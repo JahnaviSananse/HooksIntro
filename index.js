f87c7ebeae2aea771c8f1a9398f9b38d0b13094d
unknown <jahnavisananse@gmail.com>
2021-05-07 13:45:00 +0530

    useState, useReducer and custom useState

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import demoReducer from './hooksIntro/useReducer/demoReducer';
+import demoState from './hooksIntro/useReducer/demoState';
+import demoApp from './hooksIntro/Custom_useState/demoApp';
+
+AppRegistry.registerComponent(appName, () => demoApp);
