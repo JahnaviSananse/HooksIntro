4436eaa85da5e4d157550a6672ac5ec4f6f157f6
unknown <jahnavisananse@gmail.com>
2021-05-07 14:11:09 +0530

    useEffect understanding

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ import {name as appName} from './app.json';
 import demoReducer from './hooksIntro/useReducer/demoReducer';
 import demoState from './hooksIntro/useReducer/demoState';
 import demoApp from './hooksIntro/Custom_useState/demoApp';
+import demoEffect from './hooksIntro/useEffect/demoEffect';
 
-AppRegistry.registerComponent(appName, () => demoApp);
+AppRegistry.registerComponent(appName, () => demoEffect);
